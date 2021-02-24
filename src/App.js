f30d3a63db2a82c1e7a95a48724b624da9453cd9
frank liu <tianlunliu98@gmail.com>
2021-02-24 14:06:18 -0800

    landing page init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import LandingPage from './LandingPage';
 import MainPage from './main/views';
 import CreateTestTemplatePage from './main/views/CreateTestTemplatePage';
 import TestTemplateDetailsPage from './test-template-details/views';
@@ -9,7 +10,8 @@ class App extends Component {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={MainPage} />
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/tests" component={MainPage} />
           <Route exact path="/create-test-template" component={CreateTestTemplatePage} />
           <Route path="/test-template-details/:id" component={TestTemplateDetailsPage} />
           <Route render={() => <h1>Oops! Page not found!</h1>} />
